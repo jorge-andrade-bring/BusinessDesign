7601365922c0f59067db06df440cdf9eb093dffe
Jorge Andrade <jorge.andrade@bringglobal.com>
2021-02-08 13:50:13 +0000

    Initial commit

---
+++ Example/BusinessDesign/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BusinessDesign
+//
+//  Created by Jorge Andrade on 02/08/2021.
+//  Copyright (c) 2021 Jorge Andrade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
